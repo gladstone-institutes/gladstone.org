865e46dfcd646da516e36dd67a541896f37a9da9
Justin Nand <jnand@users.noreply.github.com>
2014-12-02 09:31:40 -0800

    add overlay pane style

---
+++ modules/features/gladstone_theme/plugins/styles/gladstone_overlay/gladstone-overlay-pane.tpl.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @file
+ *
+ *
+ * - $content: The content of the box.
+ */
+
+?>
+<?php if ($link): ?><a href="<?php echo $link ?>"><?php endif; ?>
+<div class="pane gladstone-overlay fpp-id-<?php echo $settings['fpid']?> clearfix <?php echo $settings['css'] ?>">
+
+	<div class="heading clearfix">
+		<h3 class="title"><?php echo $title; ?></h3>
+		<?php if (!empty($settings['icon'])): ?>
+			<i class="fa <?php echo $settings['icon'] ?>"></i>
+		<?php endif; ?>
+	</div>
+	
+	<div class="content-wrapper">
+		<img src="<?php echo $settings['bg_image_url']?>" />
+		<div class="content overlay">
+			<div class="inner">
+				<?php echo $content ?>
+			</div>
+		</div>		
+	</div>
+	
+</div>
+<?php if ($link): ?></a><?php endif; ?>
