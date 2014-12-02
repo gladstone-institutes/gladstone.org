922099207326035f1bbf29527fca14facfc94f11
Justin Nand <jnand@users.noreply.github.com>
2014-12-02 03:45:29 -0800

    gladstone media style and accompanying layout alterations

---
+++ modules/features/gladstone_theme/plugins/styles/gladstone_media/gladstone-media-pane.tpl.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @file
+ *
+ *
+ * - $content: The content of the box.
+ */
+
+?>
+<div class="pane gladstone-media-pane <?php echo $settings['css'] ?>">
+
+	<div class="heading clearfix">
+		<h3 class="title"><?php echo $title; ?></h3>
+		<?php if (!empty($settings['icon'])): ?>
+			<i class="fa <?php echo $settings['icon'] ?>"></i>
+		<?php endif; ?>
+	</div>
+
+	<?php if ($link): ?><a href="<?php echo $link ?>"><?php endif; ?>
+
+	<div class="content accent">
+		<?php echo $content; ?>
+	</div>
+
+	<?php if ($link): ?></a><?php endif; ?>	
+
+</div>
