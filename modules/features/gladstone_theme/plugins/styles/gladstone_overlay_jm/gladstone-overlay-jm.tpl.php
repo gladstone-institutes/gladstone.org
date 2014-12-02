8e777fc1abcf5befa498d3aeda2e966ee31c8355
Justin Nand <jnand@users.noreply.github.com>
2014-12-02 15:08:10 -0800

    add gladstone overlay jump menu pane style

---
+++ modules/features/gladstone_theme/plugins/styles/gladstone_overlay_jm/gladstone-overlay-jm.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @file
+ *
+ *
+ * - $content: The content of the box.
+ */
+
+?>
+<div class="pane gladstone-overlay fpp-id-<?php echo $settings['fpid']?> clearfix <?php echo $settings['css'] ?>">
+
+	<div class="heading clearfix">
+		<h3 class="title"><?php echo $title; ?></h3>
+		<?php if (!empty($settings['icon'])): ?>
+			<i class="fa <?php echo $settings['icon'] ?>"></i>
+		<?php endif; ?>
+	</div>
+	
+	<div class="content-wrapper rollover">
+		<img src="<?php echo $settings['bg_image_url']?>" />
+		<div class="content overlay">
+			<div class="inner">
+				<?php echo $content ?>
+			</div>
+			<div class="inner rollover clearfix">
+				<?php echo $menu; ?>
+			</div>
+		</div>		
+	</div>
+	
+</div>
